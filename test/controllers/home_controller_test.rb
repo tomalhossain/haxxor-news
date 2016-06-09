7a5ab7a46d97a4c53f76fb5dbd101cb882e079f1
Tomal Hossain <tomalhossain@gmail.com>
2016-06-09 15:49:14 -0600

    Second edits to home-pages branch. Ready to push again to remote orgin branch home-pages.

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,12 +1,6 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
-  test "should get home" do
-    get :home
-    assert_response :success
-    #assert_response: 
   end
-
-
 
 end
