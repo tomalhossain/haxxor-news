cb3b52890b6db319e36fc2d3761079d8ea214fd4
Tomal Hossain <tomalhossain@gmail.com>
2016-06-13 10:19:20 -0600

    reduced unnecessary commenting and tentative code, whitelists routing of controller actions, and added presence validation for posting attributes at DB level

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,17 +2,14 @@ class PostsController < ApplicationController
 
   
  
-  #to render a particular post(s).
   def show
     @post = Post.find(params[:id])
   end
   
-  #to return a view with an HTML form to create a new post
   def new
    @post = Post.new 
   end
 
-  #actually do the creating of said post
   def create
     @post = Post.new(post_params)
     if @post.save
@@ -23,35 +20,27 @@ class PostsController < ApplicationController
   end
 
   private
-    def post_params
-      params.require(:post).permit(:title, :url)
-    end
-end
+  
+  def post_params
+    params.require(:post).permit(:title, :url)
+  end
 
-=begin
+  
+=begin 
 
-  These will be filled in later...
+Actions for future tasks
 
-  #to return a view with an HTML form to edit an existing post
   def edit
   end
 
-  #to actually do the updating of said post
   def update
-  end
+  end 
 
-  #to remove a post from the database
   def destroy
-  end
+  end 
 
-  
-  #to show a list of all posts - is this even necessary
-  #if the index.html.erb file for the home controller
-  #essentially already displays a list of all the posts? 
-  def index
-  end
-  
+=end 
 
+end
 
-=end
 
--- db/migrate/20160610151323_create_posts.rb
+++ db/migrate/20160610151323_create_posts.rb
@@ -1,8 +1,8 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
-      t.string :title
-      t.string :url
+      t.string :title, null: false
+      t.string :url, null: false
       t.timestamps null: false
     end
   end
