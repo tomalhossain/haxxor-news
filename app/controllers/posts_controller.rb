c50d5e0e37a9151a297c9a9e93543383cef42673
Tomal Hossain <tomalhossain@gmail.com>
2016-06-13 17:43:34 -0600

    Eliminated unnecessary commenting

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,11 @@
 class PostsController < ApplicationController
 
-  
- 
   def show
     @post = Post.find(params[:id])
   end
-  
+
   def new
-   @post = Post.new 
+    @post = Post.new
   end
 
   def create
@@ -20,13 +18,12 @@ class PostsController < ApplicationController
   end
 
   private
-  
+
   def post_params
     params.require(:post).permit(:title, :url)
   end
 
-  
-=begin 
+=begin
 
 Actions for future tasks
 
@@ -34,12 +31,12 @@ Actions for future tasks
   end
 
   def update
-  end 
+  end
 
   def destroy
-  end 
+  end
 
-=end 
+=end
 
 end
 
