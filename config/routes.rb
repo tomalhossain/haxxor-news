c68ae85efdb42a8d0a19fd62721a367f3104f45e
Tomal Hossain <tomalhossain@gmail.com>
2016-06-15 13:47:43 -0600

    PROGRESS: Routing stuff for login

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,17 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'home#index'
+
   resources :users, only: [:new, :create, :show]
   resources :posts, only: [:new, :create, :show]
+
   get 'signup' => 'users#new'
-  get 'submit' => 'posts#new' 
+
+  get 'submit' => 'posts#new'
+
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- db/schema.rb
+++ db/schema.rb
@@ -24,11 +24,11 @@ ActiveRecord::Schema.define(version: 20160613221916) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
+    t.string   "name",            null: false
+    t.string   "email",           null: false
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.string   "password_digest"
+    t.string   "password_digest", null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
