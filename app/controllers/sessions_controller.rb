4b2b72140f4c2a90afce084de7d83c49379bd8d4
Tomal Hossain <tomalhossain@gmail.com>
2016-06-15 17:17:51 -0600

    Added login/logout functionality. Will implement cookies next.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,12 +6,16 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase) 
     if user && user.authenticate(params[:session][:password]) 
-      #log in
+      log_in user
+      redirect_to user_url(user)
     else
-      flash[:danger] = 'Invalid email/password combination'`
+      flash[:danger] = 'Invalid email/password combination'
       render 'new' 
+    end 
   end
 
   def destroy
+    log_out 
+    redirect_to root_url 
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
+      current_user
       flash[:success] = "Welcome to the Sample App!" 
       redirect_to user_url(@user) 
     else
