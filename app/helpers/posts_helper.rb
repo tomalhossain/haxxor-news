ca30173d797b3db964832b36cf832fa62232e1bb
Tomal Hossain <tomalhossain@gmail.com>
2016-06-16 15:25:31 -0600

    Cleaned up a few things here and there - mainly comments and spacing.

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -4,7 +4,6 @@ module PostsHelper
 
   def logged_in_user
     unless logged_in? 
-      #store_location
       flash[:danger] = "You must log in in order to submit a post."
       redirect_to login_url 
     end 
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,5 @@
 module UsersHelper
 
-  # Returns the Gravatar for the given user.
-
   def gravatar_for(user)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
