ef67da2999f726ebfc771809d0f7132835d0e981
Tomal Hossain <tomalhossain@gmail.com>
2016-06-24 18:12:30 -0600

    PROGRESS: Polymorphism properly set up. Now only need to figure out nested comments.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,21 +2,33 @@ class CommentsController < ApplicationController
 before_action :logged_in_user, only: [:create]
 
   def new
-  end 
+  end
 
-  def create 
-    binding.pry
-    post = Post.find(params[:format])
-    comment = post.comments.build(comment_params)
-    comment.update_attributes(user_id: current_user.id)
-    comment.save
-    redirect_to post 
-  end 
+  def create
+    @parent_post = Post.find(params[:post_id])
+    if params[:comment_id].present?
+      parent_comment = Comment.find(params[:comment_id])
+      new_comment = parent_comment.comments.build(comment_params)
+      new_comment.update_attributes(commentable_type: "Comment")
+      create_comment new_comment
+    else
+      new_comment = @parent_post.comments.build(comment_params)
+      new_comment.update_attributes(commentable_type: "Post")
+      create_comment new_comment
+    end
+  end
 
   def comment_params
     params.require(:comment).permit(:content)
   end
 
-end 
+  private
+
+  def create_comment(new_comment)
+    new_comment.update_attributes(user_id: current_user.id)
+    new_comment.save
+    redirect_to @parent_post
+  end
+end
 
 
