4bdc2fd115c315326f41a5debd90b2afbf43bb2a
Tomal Hossain <tomalhossain@gmail.com>
2016-06-30 16:28:35 -0600

    ZP's comments on pull request addressed.

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,8 +1,14 @@
 FactoryGirl.define do
 
   factory :comment do
-    content "This is a sample comment."
-    association :commentable, factory: :user
-  end
+    content Faker::Lorem.paragraph(1)
 
+    trait :for_post do
+      association :commentable, factory: :post
+    end
+
+    trait :for_comment do
+      association :commentable, factory: :comment
+    end
+  end
 end
--- spec/services/create_user_service_spec.rb
+++
@@ -1,4 +0,0 @@
-  describe 'call'
-    #valid/invalid
-    #email
-  end
