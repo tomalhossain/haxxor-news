6f63d8361d5e1bfa8825b07a68e6d89f9f9f565e
Tomal Hossain <tomalhossain@gmail.com>
2016-07-01 08:04:19 -0600

    PROGRESS: TDD setup for voting

--- spec/features/vote_spec.rb
+++ spec/features/vote_spec.rb
@@ -17,5 +17,25 @@ describe "Voting" do
       expect { click_on("-1") }.to change{Vote.count}.by(1)
     end
 
+  context "when an upvote is currently made" do
+
+    it "it destroys the vote when an upvote is cast again" do
+    end
+
+    it "modifies the value of the existing vote when a downvote is cast" do
+    end
+
+  end
+
+  context "when a downvote is currently made" do
+
+    it "it destroys the vote when a downvote is cast again" do
+    end
+
+    it "modifies the value of the existing vote when an upvote is cast" do
+    end
+
+  end
+
   end
 end
