406943793f9e515ed294fdd311e53dbc9914741a
Tomal Hossain <tomalhossain@gmail.com>
2016-07-01 13:11:18 -0600

    PROGRESS

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,18 +2,6 @@ require 'spec_helper'
 
 describe Comment, type: :model do
 
-  let!(:user)               { build :user }
-  let!(:post)               { build :post }
-  let!(:comment_for_post)   { build :comment_for_post }
-
-  it "has a valid factory" do
-    built_post = user.posts.build({ title: post.title, url: post.url })
-    built_post.save
-    binding.pry
-    built_comment = built_post.comments.build({ content: comment_for_post.content })
-    expect(built_comment).to be_valid
-  end
-
   describe '#content' do
     it { is_expected.to validate_presence_of(:content) }
   end
